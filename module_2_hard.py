1e198b698ca2ad271a3de45dbed8ee6f925f6724
livintin <natig.abdulov@gmail.com>
2024-08-14 20:23:46 +0700

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,19 +1,10 @@
-import random
-number = list(range(3, 21))
-para = list(range(1, 21))
-final = []
-n = random.choice(number)
-print(f'Число из первой вставки: {n}')
-for i in para:
-    for j in para:
-        if n % (i + j) == 0:
-            final.append(f'{i}{j}')
-unique_pairs = set()
-filtered_list = []
-for number in final:
-    pair = tuple(sorted(str(number)))
-    if pair not in unique_pairs:
-        unique_pairs.add(pair)
-        filtered_list.append(number)
-print(f'Пароль:', *filtered_list)
+result = {}
+for k in range(3, 21):
+    a = ''
+    for i in range(1, k):
+        for j in range(i + 1, k):
+            if k % (i + j) == 0:
+                a += str(i) + str(j)
+    result[k] = a
+    print(f"{k}: '{result[k]}'")
 
